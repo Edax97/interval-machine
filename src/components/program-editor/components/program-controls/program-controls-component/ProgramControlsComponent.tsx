23bb1995d0238f70d43292b9014ad679f0058122
Edwin Campos <ecamposc@uni.pe>
2022-12-28 15:40:27 -0500

    Program controls refactored.

--- src/components/program-editor/components/program-controls/program-controls-component/ProgramControlsComponent.tsx
+++ src/components/program-editor/components/program-controls/program-controls-component/ProgramControlsComponent.tsx
@@ -12,10 +12,10 @@ interface PropsType {
 
 export function ProgramControlsComponent(props: PropsType) {
   return (
-    <div className="d-flex align-items-center">
+    <div className="d-flex align-items-stretch" style={{ height: "2.5rem" }}>
       <input
-        id="program-name"
         className="form-control bg-secondary"
+        style={{ flex: "0 1 12rem" }}
         type="text"
         placeholder="Program Name"
         value={props.programName}
@@ -23,22 +23,22 @@ export function ProgramControlsComponent(props: PropsType) {
         required
       />
 
-      <div className="ms-3 ms-lg-4 me-auto d-flex">
-        <span className="input-group-text p-2 text-center">
+      <div style={{ flex: "0 0 5rem" }} className="me-auto input-group">
+        <span className="ms-2  p-2 input-group-text text-center">
           <MdRepeat className="fs-6" />
         </span>
         <input
           type="text"
-          size={1}
           className="form-control bg-secondary text-center px-2"
           value={props.loops}
           onChange={(e) => props.setLoops(+e.target.value)}
+          onSelect={(e) => e.currentTarget.select()}
         />
       </div>
 
       <button
         type="submit"
-        className="ps-3 btn p-1"
+        className="ps-3  pe-1 btn d-flex align-items-center"
         style={{ fontSize: "28px" }}
         aria-label="Save program"
       >
